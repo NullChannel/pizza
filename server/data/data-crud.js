df541ff02a2e8f56ed58ea77985701c3da50ff88
talnax <rostresh@gmail.com>
2018-01-26 12:17:31 -0800

    add data-crud.js

---
+++ server/data/data-crud.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+
+function getPizzas( callback ) {
+   try {
+
+      const folder = __dirname + 'pizzas.json';
+
+      fs.readFile( folder, function (err, data) {
+         if( err ) {
+            writeLog("Error open resource file", err.message);
+            return callback(err, null);
+         }
+         else {
+            const content = JSON.parse(data);
+            return callback(err, content);
+         }
+      });
+   }
+   catch(err) {
+      console.log('ERROR(data-crud.getPizzas())' + err );
+   }
+}
+
+function updatePizzas( callback ) {
+   try {
+
+      const folder = __dirname + 'pizzas.json';
+
+      fs.readFile( folder, function (err, data) {
+         if( err ) {
+            writeLog("Error open resource file", err.message);
+            return callback(err, null);
+         }
+         else {
+            const content = JSON.parse(data);
+            return callback(err, content);
+         }
+      });
+   }
+   catch(err) {
+      console.log('ERROR(data-crud.updatePizzas())' + err );
+   }
+}
+
+module.exports.getPizzas = getPizzas;
+module.exports.updatePizzas = updatePizzas;
